a7daec4a9c6af4a9ac4c52fe7e86dd263dc21d8b
Andrew Makar <andrew.makar77@gmail.com>
2020-05-05 19:15:22 +0300

    Implement possibility to load fonts

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,10 @@ module.exports = {
         test: /\.(png|svg|jpg|gif)$/,
         use: ['file-loader'],
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
+      },
     ],
   },
 };
