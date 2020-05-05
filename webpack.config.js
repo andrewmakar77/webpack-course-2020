3d1d42e22c7491afcafcee24b3086f97e713f477
Andrew Makar <andrew.makar77@gmail.com>
2020-05-05 14:25:10 +0300

    Setup running dev-server from src folder

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
+  devtool: 'inline-source-map',
   entry: {
     main: './src/index.js',
     analytics: './src/analytics.js',
@@ -12,7 +13,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(__dirname, 'src'),
     compress: true,
     port: 9000,
   },
